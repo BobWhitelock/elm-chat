932abbff255afbdd782bbe27b75743039f31c7ab
Bob Whitelock <bob.whitelock1@gmail.com>
2016-09-19 00:14:16 +0100

    Run elm format over code

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,4 @@
+module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as Html
@@ -5,15 +6,14 @@ import Html.Events exposing (onClick, onInput)
 import WebSocket
 
 
-
 main : Program Never
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -21,32 +21,32 @@ main =
 
 
 type alias Model =
-  { user : User
-  , nameInput : String
-  , messageInput : String
-  , messages : List Message
-  }
+    { user : User
+    , nameInput : String
+    , messageInput : String
+    , messages : List Message
+    }
 
 
 type User
-  = Anonymous
-  | Named String
+    = Anonymous
+    | Named String
 
 
 type alias Message =
-  { content : String
-  , user : String
-  }
+    { content : String
+    , user : String
+    }
 
 
 initialModel : Model
 initialModel =
-  Model Anonymous "" "" []
+    Model Anonymous "" "" []
 
 
 init : ( Model, Cmd Msg )
 init =
-  ( initialModel, Cmd.none )
+    ( initialModel, Cmd.none )
 
 
 
@@ -54,35 +54,36 @@ init =
 
 
 type Msg
-  = MessageInput String
-  | NameInput String
-  | Send
-  | NewMessage String
-  | SetName
+    = MessageInput String
+    | NameInput String
+    | Send
+    | NewMessage String
+    | SetName
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    MessageInput newInput ->
-      ({model | messageInput = newInput}, Cmd.none)
+    case msg of
+        MessageInput newInput ->
+            ( { model | messageInput = newInput }, Cmd.none )
 
-    NameInput newInput ->
-      ({model | nameInput = newInput}, Cmd.none)
+        NameInput newInput ->
+            ( { model | nameInput = newInput }, Cmd.none )
 
-    Send ->
-      ({model | messageInput = ""}, WebSocket.send "ws://echo.websocket.org" model.messageInput)
+        Send ->
+            ( { model | messageInput = "" }, WebSocket.send "ws://echo.websocket.org" model.messageInput )
 
-    NewMessage str ->
-      ({model | messages = addMessage str model}, Cmd.none)
+        NewMessage str ->
+            ( { model | messages = addMessage str model }, Cmd.none )
 
-    SetName ->
-      ({model | user = Named model.nameInput}, Cmd.none)
+        SetName ->
+            ( { model | user = Named model.nameInput }, Cmd.none )
 
 
 addMessage : String -> Model -> List Message
 addMessage content model =
-  Message content (nameOf model.user) :: model.messages
+    Message content (nameOf model.user) :: model.messages
+
 
 
 -- SUBSCRIPTIONS
@@ -90,7 +91,7 @@ addMessage content model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  WebSocket.listen "ws://echo.websocket.org" NewMessage
+    WebSocket.listen "ws://echo.websocket.org" NewMessage
 
 
 
@@ -99,41 +100,45 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ userInfo model
-    , nameInput model
-    , messages model
-    ]
+    div []
+        [ userInfo model
+        , nameInput model
+        , messages model
+        ]
 
 
 userInfo : Model -> Html Msg
 userInfo model =
-  span [] [text ("You are currently " ++ (nameOf model.user))]
+    span [] [ text ("You are currently " ++ (nameOf model.user)) ]
+
 
 nameOf : User -> String
 nameOf user =
-  case user of
-    Named  name ->
-      name
-    Anonymous ->
-      "anonymous"
+    case user of
+        Named name ->
+            name
+
+        Anonymous ->
+            "anonymous"
+
 
 nameInput : Model -> Html Msg
 nameInput model =
-  div []
-    [ input [onInput NameInput] []
-    , button [onClick SetName] [text "Set Name"]
-    ]
+    div []
+        [ input [ onInput NameInput ] []
+        , button [ onClick SetName ] [ text "Set Name" ]
+        ]
 
 
 messages : Model -> Html Msg
 messages model =
-  div []
-    [ div [] (List.reverse (List.map viewMessage model.messages))
-    , input [onInput MessageInput] []
-    , button [onClick Send] [text "Send"]
-    ]
+    div []
+        [ div [] (List.reverse (List.map viewMessage model.messages))
+        , input [ onInput MessageInput ] []
+        , button [ onClick Send ] [ text "Send" ]
+        ]
+
 
 viewMessage : Message -> Html msg
 viewMessage message =
-  div [] [ text (message.user ++ ": " ++ message.content) ]
+    div [] [ text (message.user ++ ": " ++ message.content) ]
